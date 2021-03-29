76d46214043ede01d30344a56d483d0282cde3d5
shoji <s.shoji39@gmail.com>
2021-03-29 12:07:45 +0900

    SearchTweetsServiceというクラスを定義し、searchというクラスメソッドを定義

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,11 +3,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments  # commentsテーブルとのアソシエーション
 
-  def self.search(search)
-    if search != ""
-      Tweet.where('text LIKE(?)', "%#{search}%")
-    else
-      Tweet.all
-    end
-  end
+  
 end
